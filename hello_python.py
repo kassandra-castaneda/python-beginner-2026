3e4a4bbd2eb048d86ac94386f45c5194de21b0f6
kassandra-castaneda <kassandrasophia6@gmail.com>
2026-03-14 01:20:47 +0400

    Update hello_python.py

--- hello_python.py
+++ hello_python.py
@@ -1,3 +1,16 @@
 
 
 print('Hello Python!')
+
+print('      1')
+print('    2 3')
+print('  4 5 6')
+print('7 8 9 10')
+
+print('KKKK   C') # I'm turning 16 in april!!
+print('K  K   C')
+print('K  K   C')
+print('K  K   C')
+print('K  K   C')
+print('K  K   C')
+print('KKKK   CCCCC')
