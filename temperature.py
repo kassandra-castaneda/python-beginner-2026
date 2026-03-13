c162cbe54d8a748884635a91512883242ead6f01
Kassandra Castañeda <kassandrasophia6@gmail.com>
2026-03-14 01:54:49 +0400

    Fahrenheit to Celsius Excercise

---
+++ temperature.py
@@ -0,0 +1,4 @@
+Python 3.14.3 (v3.14.3:323c59a5e34, Feb  3 2026, 11:41:37) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin
+Enter "help" below or click "Help" above for more information.
+>>> temp_f = 41
+... temp_c = (temp_f - 32) / 1.8
